75619c1b2e8a19c8205027b43c1595ab57b98389
123456514 <2670253693@qq.com>
2026-05-27 19:02:27 +0800

    first question

---
+++ lc_1_10/main.cpp
@@ -0,0 +1,22 @@
+// leetcode_1_10.cpp
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> towSum(vector<int>& nums,int target) {
+    unordered_map<int,int> map;
+    for (int i=0;i<nums.size();i++) {
+        int r = target - nums[i];
+        if (map.count(r)) {
+            return {map[r],i};
+        }
+        map[nums[i]] = i;
+    }
+}
+int main() {
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+    vector<int> res = towSum(nums,target);
+    for (int i = 0;i<res.size();i++) {
+        cout << res[i] << " ";
+    }
+    return 0;
+}
